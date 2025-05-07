1161b17f8d57e53a8520ba2fd51234e1b2b53509
Aziz Kızgın <azizkzgn@gmail.com>
2025-05-07 23:32:42 +0300

    Initial Commit

---
+++ Foody/FoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodyApp.swift
+//  Foody
+//
+//  Created by Aziz Kızgın on 7.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodyTests/FoodyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodyTests.swift
+//  FoodyTests
+//
+//  Created by Aziz Kızgın on 7.05.2025.
+//
+
+import Testing
+@testable import Foody
+
+struct FoodyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
