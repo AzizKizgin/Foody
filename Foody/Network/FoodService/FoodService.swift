e6fa9e408f13c3d37018d6ab4b5085715cfee9c2
Aziz Kızgın <azizkzgn@gmail.com>
2025-05-08 22:34:19 +0300

    feat: food service

---
+++ Foody/Network/FoodService/FoodService.swift
@@ -0,0 +1,81 @@
+//
+//  FoodService.swift
+//  Foody
+//
+//  Created by Aziz Kızgın on 8.05.2025.
+//
+
+import Foundation
+import Combine
+
+class FoodService: FoodServiceProtocol {
+    let baseURL = "www.themealdb.com/api/json/v1/1"
+    
+    func getAllCategories() -> AnyPublisher<CategoriesResponse, any Error> {
+        return URLSession.shared
+            .dataTaskPublisher(for: URL(string: "\(baseURL)/categories.php")!)
+            .map(\.data)
+            .decode(type: CategoriesResponse.self, decoder: JSONDecoder())
+            .mapError { error in
+                if error is DecodingError {
+                    return NetworkError.decodingError
+                }
+                return NetworkError.unknown
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    func getMealByCategory(category: String) -> AnyPublisher<MealsResponse, any Error> {
+        return URLSession.shared
+            .dataTaskPublisher(
+                for: URL(string: "\(baseURL)/filter.php?c=\(category)")!
+            )
+            .map(\.data)
+            .decode(type: MealsResponse.self, decoder: JSONDecoder())
+            .mapError { error in
+                if error is DecodingError {
+                    return NetworkError.decodingError
+                }
+                return NetworkError.unknown
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    func getMealByID(id: String) -> AnyPublisher<MealsResponse, any Error> {
+        return URLSession.shared
+            .dataTaskPublisher(
+                for: URL(string: "\(baseURL)/lookup.php?i=\(id)")!
+            )
+            .map(\.data)
+            .decode(type: MealsResponse.self, decoder: JSONDecoder())
+            .mapError { error in
+                if error is DecodingError {
+                    return NetworkError.decodingError
+                }
+                return NetworkError.unknown
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    func getMealByMainIngredient(ingredient: String) -> AnyPublisher<MealsResponse, any Error> {
+        return URLSession.shared
+            .dataTaskPublisher(
+                for: URL(string: "\(baseURL)/filter.php?i=\(ingredient)")!
+            )
+            .map(\.data)
+            .decode(type: MealsResponse.self, decoder: JSONDecoder())
+            .mapError { error in
+                if error is DecodingError {
+                    return NetworkError.decodingError
+                }
+                return NetworkError.unknown
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    
+}
---
+++ Foody/Network/FoodService/FoodServiceProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  FoodServiceProtocol.swift
+//  Foody
+//
+//  Created by Aziz Kızgın on 8.05.2025.
+//
+
+import Foundation
+import Combine
+
+protocol FoodServiceProtocol {
+    func getAllCategories() -> AnyPublisher<CategoriesResponse, Error>
+    func getMealByCategory(category: String) -> AnyPublisher<MealsResponse, Error>
+    func getMealByID(id: String) -> AnyPublisher<MealsResponse, Error>
+    func getMealByMainIngredient(ingredient: String) -> AnyPublisher<MealsResponse, Error>
+}
