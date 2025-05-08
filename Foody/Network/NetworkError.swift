f17dc4e3b8918b41b90fc38e0f150ec461684901
Aziz Kızgın <azizkzgn@gmail.com>
2025-05-08 22:34:34 +0300

    feat: network error enum

---
+++ Foody/Network/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  Foody
+//
+//  Created by Aziz Kızgın on 8.05.2025.
+//
+
+import Foundation
+enum NetworkError: Error {
+    case invalidURL
+    case decodingError
+    case unknown
+}
